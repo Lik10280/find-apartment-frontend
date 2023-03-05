5ae98b4a71c8a0d7d47f90198f31a39c5de8b6d3
+ <lukas.kamau@student.moringaschool.com>
2023-03-05 23:49:38 +0300

    added the rentals page to display the rentals component

---
+++ src/components/RentalsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RentalList from './RentalList';
+
+function RentalsPage() {
+  return (
+    <div>
+      <h2>Available Rentals</h2>
+      <Booking />
+    </div>
+  );
+}
+
+export default RentalsPage;
