afc72f642d480574e6f83ac472371744ac6a2844
+ <lukas.kamau@student.moringaschool.com>
2023-03-05 23:42:32 +0300

    added the about component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company that provides high-quality vacation rentals.</p>
+    </div>
+  );
+}
+
+export default About;
