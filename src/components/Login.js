270ea232d1e7143b7f766cb4a924dc86ddded35b
+ <lukas.kamau@student.moringaschool.com>
2023-03-05 23:47:44 +0300

    imported the useNavigate hook and the useState hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,68 @@
-
-import {Link,useNavigate} from "react-router-dom";
-
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Container, Row, Col, Alert } from 'react-bootstrap';
 
 function Login() {
+  const [isOwner, setIsOwner] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); 
+  const [user, setUser] = useState(null);
 
-const navigate=useNavigate()
-   
-function handleUser(e){
-  e.preventDefault()
-const email=e.target.email.value
-fetch(`http://localhost:9292/test/${email}`)
-.then(res=>{
- if(!res.ok){
- return navigate("/signup")
- }
- return  res.json()
- })
-.then(data=>{
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password
+        
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Invalid email or password");
+        }
+      })
+      .then((data) => {
+        setUser(data);
+        navigate("/rentalservice");
+        // Redirect to home page or some other page
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
   
-  navigate(`/owner/${data.id}/rentals`)})
 
-}
-
-
-return (
-    <>
-     <nav className="cartNav">
-        <div><Link to="/">Home</Link></div>
-        <div><Link to="/signup">Signup</Link></div>
-        <p style={{fontStyle:"italic",fontSize:"3rem"}}>Find A Motel</p>
-       </nav>
-    <div className="login">
-    <form  className="form" onSubmit={handleUser}>
-       <label htmlFor="email" value="Email">Email:   </label><br/>
-       <input type="email" name="email" />
-       <input type="submit" className="submit" value="Login"/>
-     </form>
-  </div>
-  </>
-  )
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs={12} md={6}>
+          <h2 className="mb-4">Login</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
