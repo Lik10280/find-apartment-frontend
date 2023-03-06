1337dc8bbdc892559896e64b0113f383842b3ad2
+ <lukas.kamau@student.moringaschool.com>
2023-03-06 06:49:30 +0300

    added the route to the reviewpage on the parent component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,26 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Signup from './Signup';
-import About from "./About";
+import About from './About';
 import RentalService from './RentalService';
 import Booking from './Booking';
-import { useState } from 'react';
-
+import ReviewForm from './ReviewForm';
+import ReviewPage from './ReviewPage';
 
 function App() {
-  const [bookingData, setBookingData] = useState({});
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/about" element={About} />
+        <Route path="/about" element={<About />} />
         <Route path="/rentalservice" element={<RentalService />} />
         <Route path="/book/:rentalId" element={<Booking />} />
-        {/* Add the new route for the booking form */}
-        
+        <Route path="/rentals/:rentalId/review" element={<ReviewForm />} />
+        <Route path="/reviews/:reviewId" element={<ReviewPage />} />
       </Routes>
     </Router>
   );
